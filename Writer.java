15335679587da3ef8066b50eb76d68113bbc9f87
Vova <demandred91@gmail.com>
2017-07-22 16:20:09 +0300

    Starting

---
+++ Writer.java
@@ -0,0 +1,23 @@
+package JSONSerializer;
+
+import java.util.List;
+
+
+public class Writer {
+
+    List<Integer> createListOfInteger(List<Integer> list){
+        return list;
+    }
+
+    List<String> createListOfString(List<String> list){
+        return list;
+    }
+
+    int createInteger(int integerValue){
+        return integerValue;
+    }
+
+    String createString(String stringValue){
+        return stringValue;
+    }
+}
